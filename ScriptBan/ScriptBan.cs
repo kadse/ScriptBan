4ff3d864187d20c643c611a6888da05472c79561
Sebastian Kaden <sebastian.kaden@s2012.tu-chemnitz.de>
2016-10-28 16:54:36 +0200

    CHA: DB - Auto Check If Table Exists & CreateTable

--- ScriptBan/ScriptBan.cs
+++ ScriptBan/ScriptBan.cs
@@ -88,16 +88,23 @@ namespace ScriptBan
                 return false;
             }
 
-            //DB Connection String
+            //Database Connection String
             string ConnStr = "server=" + config[3] + ";database=" + config[4] + ";uid=" + config[5] + ";password=" + config[6];
 
             //Database Connection Check
-            if (config[2] == "true" && !CheckDatabaseConnection(ConnStr))
+            if (config[2] == "true")
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("=> Plugin Fehler:\tScriptBan\tVerbindung zur Datenbank konnte nicht aufgebaut werden");
-                Console.ForegroundColor = ConsoleColor.Gray;
-                return false;
+                if (CheckDatabaseConnection(ConnStr))
+                {
+                    //Table Check & Create Table
+                    if (!checkTableExists(ConnStr)) { createTable(ConnStr); }
+                } else 
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("=> Plugin Fehler:\tScriptBan\tVerbindung zur Datenbank konnte nicht aufgebaut werden");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    return false;
+                }
             }
             
             client.BattlEyeMessageReceived += BattlEyeMessageReceived;
@@ -109,14 +116,14 @@ namespace ScriptBan
             return true;
         }
 
-        //Database Connection Check Function
-        private bool CheckDatabaseConnection(string CStrg)
+        //### Database Connection Check Function
+        private bool CheckDatabaseConnection(string ConStrg)
         {
             bool isConn = false;
             MySqlConnection check_conn = null;
             try
             {
-                check_conn = new MySqlConnection(CStrg);
+                check_conn = new MySqlConnection(ConStrg);
                 check_conn.Open();
                 isConn = true;
             }
@@ -126,6 +133,9 @@ namespace ScriptBan
             }
             catch (MySqlException ex)
             {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("=> Plugin Fehler:\tScriptBan\tDatabase Error #"+ex.Number.ToString());
+                Console.ForegroundColor = ConsoleColor.Gray;
                 isConn = false;
             }
             finally
@@ -138,6 +148,47 @@ namespace ScriptBan
             return isConn;
         }
 
+        //### Check Function ob Spalte banlogs existent ist
+        private static bool checkTableExists(string ConStrg)
+        {
+            MySqlConnection tableCheck_conn = new MySqlConnection(ConStrg);
+            tableCheck_conn.Open();
+
+            MySqlCommand tableCheck = tableCheck_conn.CreateCommand();
+            tableCheck.CommandText = "SELECT* FROM information_schema.tables WHERE table_schema = '" + config[4] + "' AND table_name = 'banlogs' LIMIT 1";
+            MySqlDataReader reader = tableCheck.ExecuteReader();
+            string row = "";
+            while (reader.Read())
+            {
+                row = reader.GetValue(0).ToString();
+            }
+            if (row == String.Empty) { return false; };
+
+            tableCheck_conn.Close();
+
+            return true;
+        }
+
+        //### Create Table 'banlogs' Function 
+        private static void createTable (string ConStrg)
+        {
+            MySqlConnection createTable_conn = new MySqlConnection(ConStrg);
+            createTable_conn.Open();
+
+            string query = string.Format(@"CREATE TABLE `{0}` (
+                                        `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+                                        `player` varchar(50) NOT NULL DEFAULT '',
+                                        `guid` varchar(50) NOT NULL DEFAULT '',
+                                        `logs` text NOT NULL,                                        
+                                        PRIMARY KEY (`id`))
+                                        AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8;", "banlogs");
+            MySqlCommand createTable = new MySqlCommand(query, createTable_conn);
+            createTable.ExecuteNonQuery();
+
+            createTable_conn.Close();
+        }
+
+        //### BattlEye Message Handler
         private static void BattlEyeMessageReceived(BattlEyeMessageEventArgs args)
         {
             Regex regex = new Regex(@"Player #([0-9]{1,}) (.{0,}) [(]([\w]{32})[)] has been kicked by BattlEye: (.{0,}) ([#]\d+)");
